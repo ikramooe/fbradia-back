af8e66538b0f9392575ae4b1cfd93fa08e8716a0
IKramooe <ikramatmanemi@gmail.com>
2024-05-15 13:03:36 +0100

    up

--- app/Nova/Metrics/Attestations.php
+++ app/Nova/Metrics/Attestations.php
@@ -5,6 +5,7 @@ namespace App\Nova\Metrics;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Metrics\Value;
 use Laravel\Nova\Nova;
+use App\Models\Answer;
 
 class Attestations extends Value
 {
