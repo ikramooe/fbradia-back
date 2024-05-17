48b690e2ae2c76f0650ce4401aeec225caf8c630
IKramooe <ikramatmanemi@gmail.com>
2024-05-17 18:14:23 +0100

    up

--- app/Nova/Event.php
+++ app/Nova/Event.php
@@ -7,6 +7,7 @@ use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Image;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\File;
