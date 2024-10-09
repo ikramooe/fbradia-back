023cca82ea36c48c596f5c258f59fc8063deebcc
IKramooe <ikramatmanemi@gmail.com>
2024-10-09 14:44:17 +0100

    up

--- app/Nova/Answer.php
+++ app/Nova/Answer.php
@@ -230,7 +230,7 @@ class Answer extends Resource
             
               public function actions(NovaRequest $request)
     {
-        return [ new DownloadExcel,new Actions\ImprimerBadge(), new Actions\EnvoyerAttestation(),new Actions\EnvoyerAttestationParticipation(),new Actions\EnvoyerAttestationEposter()];
+        return [  new ImportData,new DownloadExcel,new Actions\ImprimerBadge(), new Actions\EnvoyerAttestation(),new Actions\EnvoyerAttestationParticipation(),new Actions\EnvoyerAttestationEposter()];
     }
       
     
