58175cf8ff0093ec2ec87b182e07135b279d026d
IKramooe <ikramatmanemi@gmail.com>
2024-10-31 22:37:42 +0100

    up

--- app/Nova/Answer.php
+++ app/Nova/Answer.php
@@ -230,7 +230,7 @@ class Answer extends Resource
     public function filters(NovaRequest $request)
     {
         return [
-            TypeFilter::class
+            new \App\Nova\Filters\TypeFilter,
         ];
     }
 
