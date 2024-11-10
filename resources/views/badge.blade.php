91fa61d119a36f462546398356407b5db131d435
IKramooe <ikramatmanemi@gmail.com>
2024-11-11 00:49:09 +0100

    up

--- resources/views/badge.blade.php
+++ resources/views/badge.blade.php
@@ -48,6 +48,7 @@
             @php
             $answers = json_decode($evt->answers);
             $prenom = $answers->Prénom ?? $answers->prénom ?? $answers->Prenom ?? $answers->prenom ?? '';
+           $ateliers = json_decode($answers->ateliers) ?? [];
         @endphp
         
         <h1>{{ $answers->nom }}<br>{{ $prenom }}</h1>
