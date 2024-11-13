94ceee3315175e691cc3dfbfba0a8f3d4f0205a1
myorcloud <42510618+myorcloud@users.noreply.github.com>
2024-11-13 20:55:55 +0100

    Update Formulaire.php

--- app/Nova/Formulaire.php
+++ app/Nova/Formulaire.php
@@ -198,7 +198,18 @@ class Formulaire extends Resource
               
               
             ]),
-
+                Tab::make('Ateliers', [
+                  
+                    Flexible::make('Ateliers')
+                    ->addLayout('Champ', 'atelier', [
+                       
+                        Text::make('Nom'),
+                        Text::make('Code couleur','code_couleur'),
+                       
+               
+                ])
+                 
+                ]),
 
             HasMany::make('answers'),
            
