1a9f9818b28b5fc51502913acd9d8e3e24cadb8e
myorcloud <42510618+myorcloud@users.noreply.github.com>
2024-11-13 13:26:37 +0100

    Update AnswersImport.php

--- app/Imports/AnswersImport.php
+++ app/Imports/AnswersImport.php
@@ -28,7 +28,7 @@ class AnswersImport implements ToModel, WithHeadingRow
                 'nom' => $row['nom'],
                 'prenom' => $row['prenom'],
                 'email' => $row['email'],
-                'telephone' => "",
+                'telephone' => $row['telephone'],
                 'specialite' => $row['specialite'],
             ]),
         ]);
