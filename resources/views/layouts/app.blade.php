22451f9faf2b56e33eb43e8de1c251054664aa3d
myorcloud <support@orcloud.dz>
2025-07-27 10:44:45 +0100

    FIX

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
     </script>
     <!--End of Tawk.to Script-->
 
-    <div id="sfczqs216613aqxgj5q75dj6wyjrqn9x4l4"></div><script type="text/javascript" src="https://counter1.optistats.ovh/private/counter.js?c=zqs216613aqxgj5q75dj6wyjrqn9x4l4&down=async" async></script><noscript><a href="https://www.freecounterstat.com" title="hit counter"><img src="https://counter1.optistats.ovh/private/freecounterstat.php?c=zqs216613aqxgj5q75dj6wyjrqn9x4l4" border="0" title="hit counter" alt="hit counter"></a></noscript>
+    <script src="https://cdn.counter.dev/script.js" data-id="aecb578a-d265-4cff-b542-e48431114713" data-utcoffset="1"></script>
 
 </head>
 
